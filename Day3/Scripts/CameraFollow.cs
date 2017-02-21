0068de1a88adbdaf6f6a157030f22137bd0d5299
Alan <SpaceToastCoastToCoast@users.noreply.github.com>
2017-02-20 19:06:01 -1000

    Add files via upload

---
+++ Day3/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	Vector3 offset;
+	//We need to know how far away the camera needs to be from the object.
+
+	public Transform target;
+	//What is this camera focusing on?
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+		//This makes sure the camera always stays the same relative to where you put it in the Inspector.
+		//When you move the player, your camera should keep focused on it from the same distance away.
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = target.position + offset;
+		//The camera's position is constantly being set to follow the target's position.
+		//We add the offset back in so it stays far enough back for us to see our scene.
+	}
+}
---
+++ Day3/Scripts/EnemyMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	public float speed = 3.0f;
+	//Speed.
+	public Transform target;
+	//What are we chasing?
+
+	public bool isWaiting = false;
+	//We can use this to pause our enemy's movement.
+
+	Rigidbody rb;
+	Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isWaiting) {
+			//If this enemy is not waiting, then have it move.
+			Move ();
+		}
+	}
+
+	void Move() {
+		movement = target.position - transform.position;
+		//Get the distance between the target and where we are now.
+		movement.y = 0f;
+		//We don't want the enemy to fly into the air or sink through the floor, so we want no y-axis movement.
+		rb.AddForce (movement.normalized * speed, ForceMode.VelocityChange);
+		//Add force to the rigidbody to move it.
+		//"Normalized" gives us a Vector3 with its values scaled to between -1 and 1. 
+		//This makes it fair since the player's movement is calculated at the same scale.
+		rb.velocity = Vector3.ClampMagnitude (rb.velocity, speed);
+		//Finally, make sure it doesn't go faster than it is allowed to.
+	}
+}
